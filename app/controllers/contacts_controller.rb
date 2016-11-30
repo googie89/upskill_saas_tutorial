1c3ff5b6878ad5c2343957bf447297676feb77e9
Alexander Rinne <rinne1989@hotmail.com>
2016-11-30 21:14:13 +0000

    Cleand up contact form routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,10 @@ class ContactsController < ApplicationController
        @contact = Contact.new(contact_params)
        if @contact.save
          flash[:success] = "Message sent."
-         redirect_to new_contact_path
        else
-         flash[:danger] = @contact.errors.full_messages.join(", ")
-         redirect_to new_contact_path
+         flash[:error] = @contact.errors.full_messages.join(", ")
        end
+       redirect_to new_contact_path
     end
     
     private
